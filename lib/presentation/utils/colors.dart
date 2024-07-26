36b6a0c811037b6b6ee49094c459f65a80a43a77
Franky Wijanarko <frank.jr.619@gmail.com>
2024-07-26 22:50:05 +0700

    create home screen part 1 : custom app bar

--- lib/presentation/utils/colors.dart
+++ lib/presentation/utils/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyColors {
-  static const Color primaryColor = Color(0x000000ff);
+class AppColors {
+  static const Color primaryColor = Color(0xff4568dc);
   static const List<Color> primaryGradientColor = [
     Color(0xff4568dc),
     Color(0xffb06ab3),
