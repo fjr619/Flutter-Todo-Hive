e16dc82d65ef145ce5501d443c40315790ff0d2b
Franky Wijanarko <frank.jr.619@gmail.com>
2024-07-29 11:21:07 +0700

    modify home screen with separate home content

---
+++ lib/presentation/screens/home/components/home_content.dart
@@ -0,0 +1,102 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../../utils/strings.dart';
+import 'task_list_item.dart';
+
+class HomeContent extends StatelessWidget {
+  final List<int> datas;
+  final Function(int data) onSwipeDismiss;
+
+  const HomeContent(
+      {required this.datas, required this.onSwipeDismiss, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        children: [
+          /// Divider
+          const Padding(
+            padding: EdgeInsets.only(top: 10.0),
+            child: Divider(
+              thickness: 2,
+              indent: 100,
+            ),
+          ),
+
+          /// Tasks lIst
+          Expanded(
+            child: SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: datas.isNotEmpty ? _listNotEmpty() : _listEmpty(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Task list is empty
+  Widget _listEmpty() {
+    return SizedBox(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          FadeIn(
+            child: SizedBox(
+              width: 200,
+              height: 200,
+              child: Lottie.asset('assets/lottie/1.json',
+                  animate: datas.isNotEmpty ? false : true),
+            ),
+          ),
+          FadeInUp(from: 30, child: const Text(AppStr.doneAllTask))
+        ],
+      ),
+    );
+  }
+
+  /// Task list is not empty
+  Widget _listNotEmpty() {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemCount: datas.length,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (context, index) {
+        return Dismissible(
+          direction: DismissDirection.endToStart,
+          background: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.delete_outline,
+                color: Colors.grey,
+              ),
+              Gap(8),
+              Text(
+                AppStr.deleteTask,
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          onDismissed: (direction) {
+            onSwipeDismiss(datas[index]);
+          },
+          key: UniqueKey(),
+          child: TaskListItem(
+            data: datas[index],
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,9 +1,7 @@
-import 'package:animate_do/animate_do.dart';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_todo_hive/presentation/screens/home/components/task_list_item.dart';
-import 'package:flutter_todo_hive/presentation/utils/strings.dart';
-import 'package:gap/gap.dart';
-import 'package:lottie/lottie.dart';
+import 'package:flutter_todo_hive/presentation/screens/home/components/home_content.dart';
 
 import 'components/appbar.dart';
 import 'components/fab.dart';
@@ -23,101 +21,26 @@ class _HomeScreenState extends State<HomeScreen> {
     TextTheme textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
-      backgroundColor: Colors.white,
-
-      /// Custom App Bar
-      appBar: AppBar(
-        surfaceTintColor: Colors.transparent,
         backgroundColor: Colors.white,
-        toolbarHeight: 100,
-        flexibleSpace: CustomAppBar(textTheme: textTheme),
-      ),
-      floatingActionButton: const FAB(),
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            /// Divider
-            const Padding(
-              padding: EdgeInsets.only(top: 10.0),
-              child: Divider(
-                thickness: 2,
-                indent: 100,
-              ),
-            ),
 
-            /// Tasks lIst
-            Expanded(
-              child: SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: test.isNotEmpty
-
-                    /// Task list is not empty
-                    ? ListView.builder(
-                        physics: const BouncingScrollPhysics(),
-                        itemCount: test.length,
-                        scrollDirection: Axis.vertical,
-                        itemBuilder: (context, index) {
-                          return Dismissible(
-                            direction: DismissDirection.endToStart,
-                            background: const Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.delete_outline,
-                                  color: Colors.grey,
-                                ),
-                                Gap(8),
-                                Text(
-                                  AppStr.deleteTask,
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                  ),
-                                )
-                              ],
-                            ),
-                            onDismissed: (direction) {
-                              /// we will remove current task from DB
-                              setState(() {
-                                test.remove(test[index]);
-                              });
-                            },
-                            // key: Key(index.toString()),
-                            key: UniqueKey(),
-                            child: TaskListItem(
-                              data: test[index],
-                            ),
-                          );
-                        },
-                      )
-
-                    /// Task list is empty
-                    : SizedBox(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            FadeIn(
-                              child: SizedBox(
-                                width: 200,
-                                height: 200,
-                                child: Lottie.asset('assets/lottie/1.json',
-                                    animate: test.isNotEmpty ? false : true),
-                              ),
-                            ),
-                            FadeInUp(
-                                from: 30, child: const Text(AppStr.doneAllTask))
-                          ],
-                        ),
-                      ),
-              ),
-            ),
-          ],
+        /// Custom App Bar
+        appBar: AppBar(
+          surfaceTintColor: Colors.transparent,
+          backgroundColor: Colors.white,
+          toolbarHeight: 100,
+          flexibleSpace: CustomAppBar(textTheme: textTheme),
         ),
-      ),
-    );
+        floatingActionButton: const FAB(),
+        body: HomeContent(
+          datas: test,
+          onSwipeDismiss: (data) {
+            log("remove data $data");
+            setState(
+              () {
+                test.remove(data);
+              },
+            );
+          },
+        ));
   }
 }
-
